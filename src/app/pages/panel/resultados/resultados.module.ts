865b28b50ecf7b278431018d7cceacf282d67386
Cristian Carreto <carreto154@gmail.com>
2024-01-08 16:17:22 -0600

    resultados

--- src/app/pages/panel/resultados/resultados.module.ts
+++ src/app/pages/panel/resultados/resultados.module.ts
@@ -2,16 +2,26 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ResultadosComponent } from './resultados.component';
 import { ResultadosRoutingModule } from './resultados-routing.module';
+import { SharedModule } from "../../../shared/shared.module";
+import { ReactiveFormsModule } from '@angular/forms';
+import { NgxSpinnerModule } from 'ngx-spinner';
+import { NgxPaginationModule } from 'ngx-pagination';
+import { NgSelectModule } from '@ng-select/ng-select';
 
 
 
 @NgModule({
-  declarations: [
-    ResultadosComponent
-  ],
-  imports: [
-    CommonModule,
-    ResultadosRoutingModule
-  ]
+    declarations: [
+        ResultadosComponent
+    ],
+    imports: [
+        CommonModule,
+        ResultadosRoutingModule,
+        ReactiveFormsModule,
+        NgxSpinnerModule,
+        NgxPaginationModule,
+        NgSelectModule,
+        SharedModule
+    ]
 })
 export class ResultadosModule { }
