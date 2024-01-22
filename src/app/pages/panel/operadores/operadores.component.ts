f873f3916c9d87fe874c696095497bbac12f5e5c
Cristian Carreto <carreto154@gmail.com>
2024-01-22 16:40:08 -0600

    select

--- src/app/pages/panel/operadores/operadores.component.ts
+++ src/app/pages/panel/operadores/operadores.component.ts
@@ -78,6 +78,7 @@ export class OperadoresComponent implements OnInit{
       apellidoPaterno: ['', [Validators.required, Validators.minLength(2), Validators.pattern(/^([a-zA-ZÀ-ÿ\u00C0-\u00FF]{2})[a-zA-ZÀ-ÿ\u00C0-\u00FF ]+$/)]],
       apellidoMaterno: ['', [Validators.required, Validators.minLength(2), Validators.pattern(/^([a-zA-ZÀ-ÿ\u00C0-\u00FF]{2})[a-zA-ZÀ-ÿ\u00C0-\u00FF ]+$/)]],
       fechaNacimiento: ['', Validators.required],
+      secciones: [[], Validators.required],
       estatus: [true],
     });
   }
@@ -179,6 +180,7 @@ export class OperadoresComponent implements OnInit{
   agregar() {
     this.operador = this.operadorForm.value as Operadores;
     const seccionid = this.operadorForm.get('seccion')?.value;
+    const seccionesValue = this.operadorForm.get('secciones')?.value;
 
     this.operador.seccion = {id: seccionid } as Seccion;
     this.spinnerService.show();
