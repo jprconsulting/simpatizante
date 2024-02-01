e933218c6d4efd63500ddfd8e72ff32ac0002e9e
Jostzi <toquiantzi.josue@gmail.com>
2024-02-01 13:27:01 -0600

    CRUD Tipo de simpatizantes

--- src/app/core/services/indicadores.service.ts
+++ src/app/core/services/indicadores.service.ts
@@ -14,4 +14,17 @@ export class IndicadoresService {
   getAll() {
     return this.http.get<Indicadores[]>(`${this.route}/obtener-todos`);
   }
+
+  create(tipoIncidencia: Indicadores) {
+    return this.http.post(`${this.route}/crear`, tipoIncidencia);
+  }
+
+  update(id: number, tipoIncidencia: Indicadores) {
+    return this.http.put(`${this.route}/actualizar/${id}`, tipoIncidencia);
+  }
+
+  delete(id: number) {
+    return this.http.delete(`${this.route}/eliminar/${id}`);
+  }
 }
+
