f3edb6298a5c271e637729fe3c805396019d41d7
Alexis Hernandez <colores.2808.8000@gmail.com>
2024-02-23 12:16:35 -0600

    Cerrar sesion y redireccionar al login

--- src/app/core/services/http-interceptor.module.ts
+++ src/app/core/services/http-interceptor.module.ts
@@ -3,13 +3,23 @@ import { Observable, catchError, throwError } from 'rxjs'
 import { HttpEvent, HttpInterceptor, HttpHandler, HttpRequest, HttpHeaders, HttpErrorResponse } from '@angular/common/http';
 import { HTTP_INTERCEPTORS } from '@angular/common/http';
 import { Router } from '@angular/router';
+import { SecurityService } from './security.service';
+import { AppUserAuth } from 'src/app/models/login';
 
 @Injectable()
 export class HttpRequestInterceptor implements HttpInterceptor {
 
+    dataObject!: AppUserAuth | null;
+
     constructor(
-      private router: Router
+      private router: Router,
+      private securityService: SecurityService
     ) { }
+
+    setDataUser() {
+      this.dataObject = this.securityService.getDataUser();
+    }
+
     intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
         const token = localStorage.getItem('token');
         let request = req;
@@ -26,6 +36,15 @@ export class HttpRequestInterceptor implements HttpInterceptor {
         return next.handle(request).pipe(
           catchError(( err: HttpErrorResponse ) => {
             if (err.status === 401 ) {
+
+              localStorage.getItem('dataObject') && this.setDataUser();
+
+              if( this.dataObject && this.dataObject.usuarioId ){
+                this.securityService.logout( this.dataObject.usuarioId )
+                .subscribe(( ) => console.log('logout')
+                )
+              }
+
               this.router.navigateByUrl('/login');
             }
 
