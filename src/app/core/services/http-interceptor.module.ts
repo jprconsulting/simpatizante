c7804329e1b2c1a9c46565bd5f452f2c2c3bc118
Alexis Hernandez <colores.2808.8000@gmail.com>
2024-02-22 19:35:56 -0600

    Session token

--- src/app/core/services/http-interceptor.module.ts
+++ src/app/core/services/http-interceptor.module.ts
@@ -1,22 +1,38 @@
 import { Injectable, NgModule } from '@angular/core';
-import { Observable } from 'rxjs'
-import { HttpEvent, HttpInterceptor, HttpHandler, HttpRequest, HttpHeaders } from '@angular/common/http';
+import { Observable, catchError, throwError } from 'rxjs'
+import { HttpEvent, HttpInterceptor, HttpHandler, HttpRequest, HttpHeaders, HttpErrorResponse } from '@angular/common/http';
 import { HTTP_INTERCEPTORS } from '@angular/common/http';
+import { Router } from '@angular/router';
 
 @Injectable()
 export class HttpRequestInterceptor implements HttpInterceptor {
+
+    constructor(
+      private router: Router
+    ) { }
     intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-        var token = localStorage.getItem('token');
-        if (token) {
-            const headers = new HttpHeaders({
-                'Content-Type': 'application/json',
-                'Authorization': 'Bearer ' + token
+        const token = localStorage.getItem('token');
+        let request = req;
+
+        if ( token ) {
+            request = req.clone({
+                setHeaders: {
+                    authorization: `Bearer ${ token }`
+                }
             });
-            const newReq = req.clone({ headers: headers })
-            return next.handle(newReq);
-        } else {
-            return next.handle(req);
         }
+
+
+        return next.handle(request).pipe(
+          catchError(( err: HttpErrorResponse ) => {
+            if (err.status === 401 ) {
+              this.router.navigateByUrl('/login');
+            }
+
+            return throwError( err );
+          })
+        );
+
     }
 }
 
