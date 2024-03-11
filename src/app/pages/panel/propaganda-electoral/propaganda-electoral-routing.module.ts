3fd067b90b1c40c71d15906344d4a7122ead3a7f
Ruby Montes <ruby.montes.rosas@gmail.com>
2024-03-11 09:57:18 -0600

    protecionPropaganda

--- src/app/pages/panel/propaganda-electoral/propaganda-electoral-routing.module.ts
+++ src/app/pages/panel/propaganda-electoral/propaganda-electoral-routing.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { PropagandaElectoralComponent } from './propaganda-electoral.component';
+import { AuthGuard } from 'src/app/core/services/auth.guard';
 
 const routes: Routes = [
   {
     path: '',
     component: PropagandaElectoralComponent,
+    canActivate: [AuthGuard], data: { claimType: 'CanAccessPropaganda'}
   }
 ];
 
